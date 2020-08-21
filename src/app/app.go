5129b41fb9ca6b00a3508cd3ab65e6e964e41919
camolezi <camolezi@usp.br>
2020-08-21 16:02:05 -0300

    Added different logging levels and config flag

--- src/app/app.go
+++ src/app/app.go
@@ -1,27 +1,57 @@
 package app
 
 import (
-	"fmt"
+	"flag"
 	"log"
 	"net/http"
 
+	"github.com/camolezi/MicroservicesGolang/src/debug"
 	"github.com/camolezi/MicroservicesGolang/src/handlers"
 )
 
 //StartApp is the starting point of the application
 func StartApp() {
-	const port = ":8081"
+
+	addr := flag.String("addr", ":8080", "Define the port that the server will be listening and serving")
+	logLevelString := flag.String("log", "debug",
+		`Define the level of application logging: 
+		error: only error logs.
+		warning: errors and warning logs
+		debug: errors, warnings and debug logs`,
+	)
+
+	flag.Parse()
+
+	var logLevel debug.LogLevel
+	switch *logLevelString {
+	case "debug":
+		logLevel = debug.DebugLevel
+	case "warning":
+		logLevel = debug.WarningLevel
+	case "error":
+		logLevel = debug.ErrorLevel
+	default:
+		log.Fatalln("Invalid Input for log flag, Options: debug,warning,error")
+	}
+
+	logger := debug.NewLogger(logLevel)
 
 	serverMux := http.NewServeMux()
-	fmt.Println("Started server on port " + port)
+	logger.Debug().Println("Starting server on port " + *addr)
+
 	//http.HandleFunc("/", logHandler(printHello))
 	//ostHandler := withAnalytics(handlers.NewPostHandler())
 
 	serverMux.Handle("/post/", handlers.NewPostHandler())
 	//http.HandleFunc("/post", controllers.GetPost)
 
-	httpServer := &http.Server{Addr: port, Handler: serverMux}
+	httpServer := &http.Server{
+		Addr:     *addr,
+		Handler:  serverMux,
+		ErrorLog: logger.Error(),
+	}
+
 	if err := httpServer.ListenAndServe(); err != nil {
-		log.Fatal(err.Error())
+		logger.Error().Fatal(err.Error())
 	}
 }
---
+++ src/debug/log.go
@@ -0,0 +1,73 @@
+package debug
+
+import (
+	"log"
+	"os"
+)
+
+//LogLevel defines the level of logging of the aplication
+type LogLevel int
+
+const (
+	//ErrorLevel abilitates only errors logs
+	ErrorLevel LogLevel = 0
+	//WarningLevel abilitates only errors and warnings
+	WarningLevel LogLevel = 1
+	//DebugLevel abilidates- debug,warning and errors logs
+	DebugLevel LogLevel = 2
+)
+
+//Logger is the interface used for logging
+type Logger interface {
+	Warning() *log.Logger
+	Error() *log.Logger
+	Debug() *log.Logger
+}
+
+type logger struct {
+	debugLog   *log.Logger
+	warningLog *log.Logger
+	errorLog   *log.Logger
+	logLevel   LogLevel
+}
+
+func (l *logger) Warning() *log.Logger {
+	return l.warningLog
+}
+
+func (l *logger) Error() *log.Logger {
+	return l.errorLog
+}
+
+func (l *logger) Debug() *log.Logger {
+	return l.debugLog
+}
+
+//dontWrite is used when we dont want debug or warning messages
+type dontWrite struct {
+}
+
+func (*dontWrite) Write(p []byte) (n int, err error) {
+	return 0, nil
+}
+
+//NewLogger return a new usable logger
+func NewLogger(level LogLevel) Logger {
+
+	errorLog := log.New(os.Stderr, "[ERROR]  ", log.Ldate|log.Lshortfile|log.Ltime)
+
+	warningLog := log.New(&dontWrite{}, "", 0)
+	debugLog := log.New(&dontWrite{}, "", 0)
+
+	//Turn warnings on
+	if level >= 1 {
+		warningLog = log.New(os.Stderr, "[WARNING]  ", log.Ldate|log.Lshortfile|log.Ltime)
+	}
+
+	//Turn debug logs on
+	if level >= 2 {
+		debugLog = log.New(os.Stderr, "[DEBUG]  ", log.Lshortfile)
+	}
+
+	return &logger{errorLog: errorLog, warningLog: warningLog, debugLog: debugLog, logLevel: level}
+}
